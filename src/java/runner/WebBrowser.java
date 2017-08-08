db1e085b16142204b0e846873c4b85abf840c6e1
Pritam <pritamcci@gmail.com>
2017-08-08 23:13:38 +0530

    Added extension file

--- src/java/runner/WebBrowser.java
+++ src/java/runner/WebBrowser.java
@@ -23,30 +23,6 @@ public class WebBrowser {
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
         driver.manage().timeouts().setScriptTimeout(60000, TimeUnit.MILLISECONDS);
         driver.manage().timeouts().pageLoadTimeout(60000, TimeUnit.MILLISECONDS);
-
-//        System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe");
-//        System.out.println("Test");
-//        ChromeOptions options = new ChromeOptions();
-//        System.out.println("Test1");
-//        options.addArguments("test-type");
-//        options.addArguments("start-maximized");
-//        options.addArguments("--enable-automation");
-//        options.addArguments("--js-flags=--expose-gc");
-//        options.addArguments("--enable-precise-memory-info");
-//        options.addArguments("test-type=browser");
-//        options.addArguments("disable-infobars");
-//        System.out.println("Test3");
-//        driver = new ChromeDriver(options);
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        System.out.println("Test4");
-//        System.out.println(driver);
-//        driver.navigate().to("www.google.com");
-//        System.out.println("Test5");
-
-        ////FirefoxDriver
-        //System.setProperty("webdriver.firefox.marionette","C:\\Driver\\geckodriver.exe");
-//        driver = new FirefoxDriver();
-//        driver.navigate().to("www.google.com");
     }
 
     @After
