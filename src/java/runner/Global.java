04e4d7891994f82ef96256f2907f108305a82f28
Pritamchibde <Pritamchibde@users.noreply.github.com>
2017-09-12 12:41:25 +0530

    Update Global.java

--- src/java/runner/Global.java
+++ src/java/runner/Global.java
@@ -3,8 +3,4 @@ package runner;
 public class Global {
 
     public static String FaceBookUrl = "https://www.facebook.com/";
-
-    public static String Username = "Coolprit13@gmail.com";
-
-    public static String Password = "Aut0m@t!onProject";
 }
